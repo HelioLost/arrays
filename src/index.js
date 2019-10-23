e546cd9d781eece8587167e68737ea05fab35d38
Jeff <Jeff.t.nobl@gmail.co>
2019-10-23 13:45:46 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,61 @@
 import Senators from './data/senators'
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
+  const repub = Senators.filter(x => x.party == "Republican");
+  return repub;
 }
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
+  const demo = Senators.filter(x => x.party == "Democrat");
+  return demo;
 }
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
+  const indep = Senators.filter(x => x.party == "Independent");
+  return indep;
 }
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
+  const sex = Senators.filter(x => x.person.gender == "male");
+  return sex;
 }
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
+  const sex = Senators.filter(x => x.person.gender == "female");
+  return sex;
 }
 
 export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
+  const sta = Senators.filter(x => x.state == "UT");
+  return sta;
 }
 
 export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
-}
+
+ let filteredSenators = Senators.map(p => { 
+
+   let obj = {}
+   obj.firstName = p.person.firstName;
+   obj.lastName = p.person.lastName;
+   obj.party = p.party;
+   obj.gender = p.person.gender;
+   return obj})
+   return filteredSenators
+  }
+//  let sen = Senators.map(p => ({k:v,k:v}))
+// `${obj.firstname = p.person.firstname} ${obj.lastname = p.person.lastname} ${obj.party = p.party} ${obj.gender = p.person.gender}` );
+//  , {})
+// }
+
+ 
 
 export const reducedCount = () => {
-  return REPLACE_ME_WITH_CODE
+let z = Senators.reduce( (x, t) => {
+x[t.party.toLowerCase()]=(x[t.party.toLowerCase()]|| 0)+1;
+return x;
+
+} , {})
+return z;
 }
 
-
-const REPLACE_ME_WITH_CODE = false
+ const REPLACE_ME_WITH_CODE = false
